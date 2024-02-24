f01822519409862ced4bf60dd6cb5bbabf612f7d
adeel-atta <adeelatta2000@gmail.com>
2024-02-24 15:18:13 +0500

    progress update

--- App.js
+++ App.js
@@ -1,26 +1,65 @@
-import React,{useState} from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView,TextInput } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  TextInput,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState(``);
 
-  const [text,setText] = useState(``);
   return (
     <>
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native </Text>
-      <Text>Type Your Name </Text>
-      
-      <TextInput keyboardType='email-address'
-      style={styles.input}
-      placeholder='Type your Name here'
-      onChangeText={(newText)=>{setText(newText)}}
-      defaultValue={text}
+      <View style={[styles.container]}>
+        <Text style={styles.title}>Welcom ReactNative </Text>
+        <TouchableOpacity
+          onPress={() => {
+            //  alert(`Button Taped`)
+            
+          }}
+        >
+          <Image
+            margin={10}
+            borderRadius={10}
+            borderWidth={10}
+            fadeDuration={1000}
+            source={{
+              width: 100,
+              height: 100,
+              uri: `https://picsum.photos/200`,
+            }}
+          />
+        </TouchableOpacity>
 
-      />
+        <Text>Type Your Name!</Text>
 
-      {text && text.length > 0 && <Text>Hello {text} </Text>}
-    </View>
+        <TextInput
+          keyboardType="email-address"
+          style={styles.input}
+          placeholder="Type your Name here"
+          onChangeText={(newText) => {
+            setText(newText);
+          }}
+          defaultValue={text}
+        />
+        <Button onPress={()=>{
+          text.length > 0 ?
+          (Alert.alert(`Welcome`, `Hello ${text}`))
+          :
+          (Alert.alert(`ERROR`, `No Name Exist`))
+        }
+          
+        } title="Click me" />
+
+      </View>
     </>
   );
 }
@@ -28,26 +67,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:20,
-    padding:10,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title:{
-    borderWidth:2,
+  title: {
+    // borderWidth: 2,
     // paddingTop:10,
-    paddingVertical:10,
-    paddingHorizontal:5,
-    color:`red`
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    color: `red`,
   },
-  input:{
-    height:40,
-    borderWidth:2,
-    marginHorizontal:5,
-    marginVertical:10,
-    borderStartColor:`red`,
-    borderEndColor:`green`,
-    borderRadius:10,
-  }
+  input: {
+    width: `100%`,
+    padding: 10,
+    fontSize: 18,
+    borderWidth: 2,
+    marginHorizontal: 5,
+    marginVertical: 10,
+    borderStartColor: `red`,
+    borderEndColor: `green`,
+    borderRadius: 10,
+  },
 });
--- README.txt
+++ README.txt
@@ -8,12 +8,18 @@ Creating DoneWithit app that is marketplace for selling things that you are done
 ✔- create Repository on Github
 ✔ - Update initial code to Github Repository
 ✔ - Overview Folder Structutre
-- - Basic Fundamental Concepts
-    ✔ - View   -> similar as div
-    ✔ -Text -> wrap text with this.
-    ✔ -TextInput -> get user Input
-
-
+- - Basic Fundamental Concepts  
+    import { ... } from 'react-native';
+    ✔ - View   -> similar as div           Style={{}},
+    ✔ -Text -> wrap text with this.        numberOfLines={1}, onPress={()=>{}} 
+    ✔ -TextInput -> get user Input.        placeHolder=``, defaultValue=``, onChangeText={()=>{}}, keyboardType=``
+    ✔ -Image -> for Images.                source={{width,height,uri}}, fadeDuration, etc etc
+    ✔ -Touchables -> used when one component has no touch features, like images, TouchableWithoutFeedback, TouchableOpacity (reduces opacity while touch)...etc
+    ✔ -Buttons
+    ✔ -Alerts
+    ✔ -styles &&  StyleSheet  => benefit of using StyleSheet instead of js Objects or incine styles 
+    
+    
 
 
 Current View:
