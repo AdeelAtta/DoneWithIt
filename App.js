74c4814fcfc0a3b017c68c9831347331a80df88d
adeel-atta <adeelatta2000@gmail.com>
2024-02-24 14:08:55 +0500

    progress

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import React,{useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,TextInput } from 'react-native';
 
 export default function App() {
+
+  const [text,setText] = useState(``);
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello React Native </Text>
+      <Text>Type Your Name </Text>
+      
+      <TextInput keyboardType='email-address'
+      style={styles.input}
+      placeholder='Type your Name here'
+      onChangeText={(newText)=>{setText(newText)}}
+      defaultValue={text}
+
+      />
+
+      {text && text.length > 0 && <Text>Hello {text} </Text>}
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop:20,
+    padding:10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  title:{
+    borderWidth:2,
+    // paddingTop:10,
+    paddingVertical:10,
+    paddingHorizontal:5,
+    color:`red`
+  },
+  input:{
+    height:40,
+    borderWidth:2,
+    marginHorizontal:5,
+    marginVertical:10,
+    borderStartColor:`red`,
+    borderEndColor:`green`,
+    borderRadius:10,
+  }
 });
--- README.txt
+++ README.txt
@@ -7,3 +7,19 @@ Creating DoneWithit app that is marketplace for selling things that you are done
 ✔ -Install APP using expo CLI
 ✔- create Repository on Github
 ✔ - Update initial code to Github Repository
+✔ - Overview Folder Structutre
+- - Basic Fundamental Concepts
+    ✔ - View   -> similar as div
+    ✔ -Text -> wrap text with this.
+    ✔ -TextInput -> get user Input
+
+
+
+
+Current View:
+
+
+![Current View](`./assets/status/1.PNG`)
+
+
+
